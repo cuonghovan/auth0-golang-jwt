026fb37473a87fd79d990cd36cf9da7de5379327
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-08-15 17:06:16 +0700

    Create and validate token

--- main.go
+++ main.go
@@ -1,90 +1,172 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/auth0-community/auth0"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	jose "gopkg.in/square/go-jose.v2"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/graphql-go/graphql"
 )
 
-type Product struct {
-	Id          int
-	Name        string
-	Slug        string
-	Description string
+type User struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-var products = []Product{
-	Product{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters", Description: "Shoot your way to the top on 14 different hoverboards"},
-	Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
-	Product{Id: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
-	Product{Id: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
-	Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
-	Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
+type Blog struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	Author    string `json:"author"`
+	Pageviews int32  `json:"pageviews"`
+}
+
+var jwtSecret []byte = []byte("thepolyglotdeveloper")
+
+var accountsMock []User = []User{
+	User{
+		Id:       "1",
+		Username: "nraboy",
+		Password: "1234",
+	},
+	User{
+		Id:       "2",
+		Username: "mraboy",
+		Password: "5678",
+	},
+}
+
+var blogsMock []Blog = []Blog{
+	Blog{
+		Id:        "1",
+		Author:    "nraboy",
+		Title:     "Sample Article",
+		Content:   "This is a sample article written by Nic Raboy",
+		Pageviews: 1000,
+	},
+}
+
+var accountType *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Account",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"username": &graphql.Field{
+			Type: graphql.String,
+		},
+		"password": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
+
+var blogType *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Blog",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"title": &graphql.Field{
+			Type: graphql.String,
+		},
+		"content": &graphql.Field{
+			Type: graphql.String,
+		},
+		"author": &graphql.Field{
+			Type: graphql.String,
+		},
+		"pageviews": &graphql.Field{
+			Type: graphql.Int,
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				_, err := ValidateJWT(params.Context.Value("token").(string))
+				if err != nil {
+					return nil, err
+				}
+				return params.Source.(Blog).Pageviews, nil
+			},
+		},
+	},
+})
+
+func ValidateJWT(t string) (interface{}, error) {
+	if t == "" {
+		return nil, errors.New("Authorization token must be present")
+	}
+	token, _ := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+		return jwtSecret, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println("claims", claims)
+		// TODO: Return user where userid is the same in claims
+		return accountsMock[1], nil
+	} else {
+		return nil, errors.New("Invalid authorization token")
+	}
+}
+
+func CreateToken(user User) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+	})
+	tokenString, error := token.SignedString(jwtSecret)
+	if error != nil {
+		fmt.Println(error)
+	}
+	return tokenString
+}
+
+func Login(response http.ResponseWriter, request *http.Request) {
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	// TODO: validate username, password  before generate token.
+
+	token := CreateToken(user)
+	response.Header().Set("content-type", "application/json")
+	response.Write([]byte(`{ "token": "` + token + `" }`))
 }
 
 func main() {
-	r := mux.NewRouter()
-
-	r.Handle("/", http.FileServer(http.Dir("./views/")))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-
-	r.Handle("/products", authMiddleware(ProductsHandler)).Methods("GET")
-	r.Handle("/products/{slug}/feedback", authMiddleware(AddFeedbackHandler)).Methods("POST")
-
-	http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
-}
-
-func authMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		secret := []byte("{YOUR-AUTH0-API-SECRET}")
-		secretProvider := auth0.NewKeyProvider(secret)
-		audience := []string{"{YOUR-AUTH0-API-AUDIENCE}"}
-
-		configuration := auth0.NewConfiguration(secretProvider, audience, "https://{YOUR-AUTH0-DOMAIN}.auth0.com/", jose.HS256)
-		validator := auth0.NewValidator(configuration)
-
-		token, err := validator.ValidateRequest(r)
-
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Token is not valid:", token)
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Unauthorized"))
-		} else {
-			next.ServeHTTP(w, r)
-		}
+	fmt.Println("Starting the application at :12345...")
+	rootQuery := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"account": &graphql.Field{
+				Type: accountType,
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					account, err := ValidateJWT(params.Context.Value("token").(string))
+					if err != nil {
+						return nil, err
+					}
+					return account, nil
+				},
+			},
+			"blogs": &graphql.Field{
+				Type: graphql.NewList(blogType),
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					return blogsMock, nil
+				},
+			},
+		},
 	})
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	http.HandleFunc("/graphql", func(response http.ResponseWriter, request *http.Request) {
+		result := graphql.Do(graphql.Params{
+			Schema:        schema,
+			RequestString: request.URL.Query().Get("query"),
+			Context:       context.WithValue(context.Background(), "token", request.URL.Query().Get("token")),
+		})
+		json.NewEncoder(response).Encode(result)
+	})
+	http.HandleFunc("/login", Login)
+	http.ListenAndServe(":12345", nil)
 }
-
-var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	payload, _ := json.Marshal(products)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(payload))
-})
-
-var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	var product Product
-	vars := mux.Vars(r)
-	slug := vars["slug"]
-
-	for _, p := range products {
-		if p.Slug == slug {
-			product = p
-		}
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if product.Slug != "" {
-		payload, _ := json.Marshal(product)
-		w.Write([]byte(payload))
-	} else {
-		w.Write([]byte("Product Not Found"))
-	}
-})
